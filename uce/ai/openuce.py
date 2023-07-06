b75da8ab46e66ce6b1e31a4c7b8316bf566b56b2
Andrea <alvargas@uce.edu.ec>
2023-07-06 00:50:51 -0500

    Primer commit

--- uce/ai/openuce.py
+++ uce/ai/openuce.py
@@ -1,12 +1,12 @@
 import openai
 from pydantic import BaseModel
 
-openai.organization = 'tu key empresa'
-openai.api_key = 'tu key personal'
+openai.organization = 'org-qlfeJ5qRmqdxY6Vi6lA8OI68'
+openai.api_key = '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'
 
 
 class Document(BaseModel):
-    item: str = 'pizza'
+    item: str = 'factorial de un numero'
 
 
 def process_inference(user_prompt) -> str:
@@ -14,14 +14,33 @@ def process_inference(user_prompt) -> str:
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": """Eres un chef que lista los ingredientes de los platillos que se te proporcionan.
+            {"role": "system", "content": """Eres un profesor de programaciòn, nivel universitario, enseñas como programar
         E.G
-        pan
-        Ingredientes:
-        arina
-        huevos
-        agua
-        azucar
+        Calcular promedios de notas de los estudiantes
+        CODIGO:
+        # Importar librerías necesarias
+    import numpy as np
+    import pandas as pd
+
+# Definir una función para calcular el promedio
+    def calcular_promedio(notas):
+    promedio = np.mean(notas)
+    return promedio
+
+# Leer archivo de notas
+    notas = pd.read_csv('notas.csv')
+
+# Calcular promedio de cada estudiante
+    promedios = []
+    for i in range(len(notas)):
+    promedio = calcular_promedio(notas.iloc[i, 1:])
+    promedios.append(promedio)
+
+# Agregar columna de promedios al archivo de notas
+    notas['Promedio'] = promedios
+
+# Guardar archivo con los promedios
+    notas.to_csv('notas_con_promedio.csv', index=False)
         ...
         """},
             {"role": "user", "content": user_prompt}
